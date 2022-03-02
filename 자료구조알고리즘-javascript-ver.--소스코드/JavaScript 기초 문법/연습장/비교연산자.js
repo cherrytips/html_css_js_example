61b4a0f09f73de98df2a8a8f6eaa60af71881e87
cherrytips <cherrytips24@gmail.com>
2022-03-02 14:26:12 +0900

    비교연산자 완료

---
+++ 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/비교연산자.js
@@ -0,0 +1,15 @@
+console.log("2" > "A"); // false
+
+console.log("Hello" < "Hi"); // true
+
+console.log("Hello" >= "Helloo"); // false
+
+console.log("5" <= 10); // true
+
+console.log(true == 1); // true
+
+console.log(false != 123); // true
+
+console.log(true === 1); // false
+
+console.log(false !== 123); // true
