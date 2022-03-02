3167af08b3af7f19b2dbb98163bf3adf737fc692
cherrytips <cherrytips24@gmail.com>
2022-03-02 13:55:50 +0900

    복합대입연산자 완료

---
+++ 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/복합대입연산자.js
@@ -0,0 +1,24 @@
+let num = 10;
+
+let result_1, result_2, result_3, result_4, result_5;
+result_1 = result_2 = result_3 = result_4 = result_5 = 31;
+
+// 복합 대입 연산자: +=
+result_1 += num;
+console.log(result_1); // 41
+
+// 복합 대입 연산자: -=
+result_2 -= num;
+console.log(result_2); // 21
+
+// 복합 대입 연산자: *=
+result_3 *= num;
+console.log(result_3); // 310
+
+// 복합 대입 연산자: /=
+result_4 /= num;
+console.log(result_4); // 3.1
+
+// 복합 대입 연산자: %=
+result_5 %= num;
+console.log(result_5); // 1
