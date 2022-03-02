5cf802cdb8fe91db719f0b01cbaf83718adf6639
cherrytips <cherrytips24@gmail.com>
2022-03-02 14:13:00 +0900

    증가감소연산자 완료

---
+++ 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/증가감소연산자.js
@@ -0,0 +1,21 @@
+let num, result;
+
+num = 10;
+result = num++;
+console.log(result); // 10
+console.log(num); // 11
+
+num = 10;
+result = ++num;
+console.log(result); // 11
+console.log(num); // 11
+
+num = 10;
+result = num--;
+console.log(result); // 10
+console.log(num); // 9
+
+num = 10;
+result = --num;
+console.log(result); // 9
+console.log(num); // 9
