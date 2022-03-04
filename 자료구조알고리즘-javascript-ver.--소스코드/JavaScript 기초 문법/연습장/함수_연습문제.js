41b2734c80335db96c5ae7e44b1f4312bf458c4b
cherrytips <cherrytips24@gmail.com>
2022-03-04 11:49:38 +0900

    함수 연습문제 완료 (#23)
    - 두 정수를 입력받아 가장 큰 값을 출력하는 함수를 작성하시오.

---
+++ 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/함수_연습문제.js
@@ -0,0 +1,10 @@
+// 두 정수를 입력받아 가장 큰 값을 출력하는 함수를 작성하시오.
+function MAX(x, y) {
+  return x > y ? x : y;
+}
+
+console.log(MAX(0, 3)); // 3 출력
+
+console.log(MAX(-1, 5)); // 5 출력
+
+console.log(MAX(100, 7)); // 100 출력
