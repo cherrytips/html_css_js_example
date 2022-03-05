7fe6bde7fcaef351ae81c26896bd56a61d671331
cherrytips <cherrytips24@gmail.com>
2022-03-05 09:44:18 +0900

    Number 자료형 완료 (#26)
    * Number 자료형 완료
    - 지수, 진법
    - 형 변환: isNaN, isFinite, parseInt, parseFloat

---
+++ 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/Number_지수_진법.js
@@ -0,0 +1,13 @@
+// --------------- 지수
+let billion_1 = 1000000000; // 10억
+let billion_2 = 1e9; // 1 + 0 9개
+let us = 1e-6; // micro sec, 왼쪽으로 6번 소수점 이동
+
+console.log(billion_1); // 1000000000 출력
+console.log(billion_2); // 1000000000 출력
+console.log(us); // 0.000001 출력
+
+// --------------- 진법
+console.log(0x0f); // 15
+console.log(0o17); // 15
+console.log(0b1111); // 15
