dbe69e830c318c22692f6cb51034be1cb65f6e61
cherrytips <cherrytips24@gmail.com>
2022-03-06 13:02:56 +0900

    고차함수 reduce - minor fix

--- 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/고차함수_reduce.js
+++ 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/고차함수_reduce.js
@@ -2,6 +2,7 @@ let nums = [1, 2, 3, 4, 5];
 let call_count = 0;
 
 console.log("result\tvalue\tindex");
+
 let sum = nums.reduce((accumulator, item, index, array) => {
   console.log(accumulator, "\t\t", item, "\t\t", index);
   call_count++;
