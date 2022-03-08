17c8da5c2ffdeb4b9336657d948272809dfe6cdf
cherrytips <cherrytips24@gmail.com>
2022-03-08 14:42:14 +0900

    Date 완료

---
+++ 자료구조알고리즘-javascript-ver.--소스코드/JavaScript 기초 문법/연습장/Date_benchmark.js
@@ -0,0 +1,22 @@
+function dateSub(old_date, new_date) {
+  return new_date - old_date;
+}
+
+function getTimeSub(old_date, new_date) {
+  return new_date.getTime() - old_date.getTime();
+}
+
+function benchmark(callback_func) {
+  let date_1 = new Date("2022-01-01");
+  let date_2 = new Date();
+
+  let start = Date.now();
+  for (let i = 0; i < 100000; i++) {
+    callback_func(date_1, date_2);
+  }
+  return Date.now() - start;
+}
+
+console.log("dateSub: " + benchmark(dateSub) + "ms");
+console.log("getTimeSub: " + benchmark(getTimeSub) + "ms");
+// 밀리세컨드로 변환해 계산하는것이 빠르다.
