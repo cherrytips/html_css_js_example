902d2e2cc23d4161d989a34a22b3d4bb8639e3e4
cherrytips <cherrytips24@gmail.com>
2022-03-15 16:58:28 +0900

    배열 4번 완료

---
+++ 자료구조알고리즘-javascript-ver.--소스코드/선형 자료구조/선형자료구조_04배열문제풀이_체스세트/main.js
@@ -0,0 +1,31 @@
+/* 체스 세트 */
+
+/* user code */
+function answer(chess) {
+  let result = [];
+  let require = [1, 1, 2, 2, 2, 8];
+
+  for (let i = 0; i < chess.length; i++) {
+    if (chess[i] === require[i]) result[i] = 0;
+    else result[i] = require[i] - chess[i];
+  }
+
+  return result;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  [0, 1, 2, 2, 2, 7],
+
+  // TC: 2
+  [2, 1, 2, 1, 2, 1],
+
+  // TC: 3
+  [0, 1, 1, 5, 3, 6],
+];
+
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(answer(input[i]));
+}
