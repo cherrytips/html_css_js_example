a67593c06646daaa15d68da811c0889a4700e0dd
cherrytips <cherrytips24@gmail.com>
2022-03-17 09:31:59 +0900

    기본문법 완료

--- javascript기본-소스코드/03기본문법/기본문법_06연산자.js
+++ javascript기본-소스코드/03기본문법/기본문법_06연산자.js
@@ -32,6 +32,7 @@ var a6 = false && "Cat"; // f && t returns false
 var a7 = "Cat" && false; // t && f returns false
 var a8 = true && true && true && true; // t && t && t && t returns true
 var a9 = true && true && false && true && true; // t && t && f && t && t returns false
+console.log(a1, a2, a3, a4, a5, a6, a7, a8, a9);
 
 // OR 연산자
 var o1 = true || true; // t || t returns true
@@ -42,6 +43,7 @@ var o5 = "Cat" || "Dog"; // t || t returns Cat
 var o6 = false || "Cat"; // f || t returns Cat
 var o7 = "Cat" || false; // t || f returns Cat
 var o8 = false || false || false || false || true; // f || f || f || f || t returns true
+console.log(o1, o2, o3, o4, o5, o6, o7, o8);
 
 // NOT 연산자
 var n1 = !true; // !t returns false
@@ -54,7 +56,7 @@ const temp = 1 === "1" ? "참일때" : "거짓일때";
 let temp2;
 
 if (1 === "1") {
-	temp2 = "참일때";
+  temp2 = "참일때";
 } else {
-	temp2 = "거짓일때";
+  temp2 = "거짓일때";
 }
--- javascript기본-소스코드/03기본문법/기본문법_08문.js
+++ javascript기본-소스코드/03기본문법/기본문법_08문.js
@@ -1,20 +1,22 @@
 /**
  * 문
  */
-if (10 > 1) {
-	return '10은 1보다 크다';
-} else if (10 === 10) {
-	return '';
-} else {
-	return '';
+function statement() {
+  if (10 > 1) {
+    return '10은 1보다 크다';
+  } else if (10 === 10) {
+    return '';
+  } else {
+    return '';
+  }
 }
 
 const array = [1, 2, 3];
 
 // 초기화; 평가(참 => 실행, 거짓 => 다음으로); 매번 실행되는 평가식;
 for (let i = 0; i < array.length; i++) {
-	console.log(i);
-	console.log(array[i]);
+  console.log(`${i}: ${array[i]}`);
+  console.log(i + ': ' + array[i]);
 }
 
 console.log(array[0]);
@@ -22,9 +24,9 @@ console.log(array[1]);
 console.log(array[2]);
 
 if (array.length === 3) {
-	for (let i = 0; i < array.length; i++) {
-		if (array[i] === 1) {
-			console.log(array[i]);
-		}
-	}
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 1) {
+      console.log(array[i]);
+    }
+  }
 }
